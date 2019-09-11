dc5907a19c476541dbea8e7e5d21b18ee2ea90c0
yospig <hotaquariusplus@gmail.com>
2019-09-11 12:05:54 +0900

    リファクタ

--- main/main.go
+++ main/main.go
@@ -22,32 +22,35 @@ import (
 var conf = map[string]string{}
 
 var (
+	// GCPパラメータ
+	gcpProjectId 	    string // GCP project ID
+	gsBucketName		string // Bucket Name of Google Cloud Storage
 	gsFileURI           string // URI, target file on Google Cloud Storage
-	projectId 	        string // GCP project ID
-	bucketName			string // Bucket Name of Google Cloud Storage
-	workDir				string // working directory name on Bucket
-	localConf   	    string // local conf file
-	resultFile          string //
-	localFile			string // original file path
+	gsWorkDir			string // working directory name on Bucket
+
 	voiceFile           string // original filename
-	voiceFileNameNonExt string // original filename without ext
 )
 
+const SAMPLING_RATE_HERTZ = 44100
+const LANGUAGE_CODE = "ja-JP"
+
 func main() {
-	// voice file upload
-	result := uploadFile()
-	if result != nil {
-		log.Fatalf("failed to uploadFile: %v", result)
+	// 音声ファイルのアップロード
+	err := uploadFile()
+	if err != nil {
+		log.Fatalf("failed to uploadFile: %v", err)
 	}
 
-	// create response file
-	blankFile, _ := os.Create(fmt.Sprintf("RespText_%s.txt", voiceFileNameNonExt))
-	defer blankFile.Close()
-
-	result = reqCloudSpeechToText(blankFile, gsFileURI)
-	if result != nil {
-		log.Fatalf("failed to reqCloudSpeechToText: %v", result)
+	// レスポンスファイル作成
+	voiceFileNameNonExt := getVoiceFileName(voiceFile)
+	textFile := fmt.Sprintf("RespText_%s.txt", voiceFileNameNonExt)
+	respFile, _ := os.Create(textFile)
+	defer respFile.Close()
+	err = reqCloudSpeechToText(respFile, gsFileURI)
+	if err != nil {
+		log.Fatalf("failed to reqCloudSpeechToText: %v", err)
 	}
+	log.Printf("Transcription completed ! : %s\n", textFile)
 }
 
 // init 初期処理の実施
@@ -67,99 +70,13 @@ func init() {
 	// read a voice file
 	vf, err := filepath.Abs(fp)	// "//User/local/xxxx.flac"
 	if err != nil{
-		log.Fatalf("local File not found")
+		log.Fatalf("local voice file not found")
 	}
-	localFile = vf
-	voiceFile = filepath.Base(localFile)
-	voiceFileNameNonExt = getVoiceFileName(voiceFile) // something voice file
+	voiceFile = filepath.Base(vf)
 
-	// config.yamlの読み込み
-	LoadConf()
-
-	return
-}
-
-// uploadFile upload voice file to Google Cloud Storage
-func uploadFile() error {
-	f, err := os.Open(localFile)
-	if err != nil{
-		return err
-	}
-	defer f.Close()
-
-	ctx := context.Background()
-	client, err := storage.NewClient(ctx)
-	if err != nil {
-		log.Fatalf("failed to create GS client: %v", err)
-	}
-	wc := client.Bucket(bucketName).Object(workDir + voiceFile).NewWriter(ctx)
-	if _, err = io.Copy(wc, f); err != nil {
-		return err
-	}
-	if err := wc.Close(); err != nil {
-		return err
-	}
-	return nil
-}
-
-// reqCloudSpeechToText CloudSpeechToTextへのリクエストを実行する
-func reqCloudSpeechToText(w io.Writer, gcsURI string) error {
-	// 空のコンテキストを生成。ゴールーチンのタイムアウト、キャンセルなどの実装に利用する。
-	ctx := context.Background()
-
-	// Creates a client
-	client, err := speech.NewClient(ctx)
-	if err != nil {
-		return err
-	}
-
-	// Detects speech in the audio file
-	// FLACエンコード、サンプリングレート44100Hz、日本語
-	req := &speechpb.LongRunningRecognizeRequest{
-		Config: &speechpb.RecognitionConfig{
-			Encoding:        speechpb.RecognitionConfig_FLAC,
-			SampleRateHertz: 44100,
-			LanguageCode:    "ja-JP",
-		},
-		Audio: &speechpb.RecognitionAudio{
-			AudioSource: &speechpb.RecognitionAudio_Uri{Uri: gcsURI},
-		},
-	}
-
-	op, err := client.LongRunningRecognize(ctx, req)
-	if err != nil {
-		return err
-	}
-	resp, err := op.Wait(ctx)
-	if err != nil {
-		return err
-	}
-	// Prints to the results.
-	for _, result := range resp.Results {
-		for _, alt := range result.Alternatives {
-			fmt.Fprintf(w, "\"%v\" (confidence=%3f)\n", alt.Transcript, alt.Confidence)
-		}
-	}
-	return nil
-}
-
-
-func exists(filename string) bool {
-	_, err := os.Stat(filename)
-	return err == nil
-}
-
-// getVoiceFileName ファイルから拡張子を取り除いたファイル名を取得
-func getVoiceFileName(f string) string {
-	slist := strings.Split(f, ".")
-	return slist[0]
-}
-
-
-// LoadConf load config
-func LoadConf() {
+	// ローカルコンフィグファイルの読み込み
 	var c map[string]interface{}
-	localConf, _ = filepath.Abs("./conf/config.yaml") //"./conf/config.yaml"
+	localConf, _ := filepath.Abs("./conf/config.yaml") //"./conf/config.yaml"
 	buf, err := ioutil.ReadFile(localConf)
 	if err != nil {
 		log.Printf("%s is not exists", localConf)
@@ -172,19 +89,93 @@ func LoadConf() {
 	}
 	id, ok := c["project-id"]
 	if ok == true {
-		projectId = id.(string)
+		gcpProjectId = id.(string)
 	}
 	bname, ok := c["bucket-name"]
 	if ok == true {
-		bucketName = bname.(string)
+		gsBucketName = bname.(string)
 	}
 	dir, ok := c["work-dir"]
 	if ok == true {
-		workDir = dir.(string)
+		gsWorkDir = dir.(string)
 	}
 	gs, ok := c["gs"]
 	if ok == true {
-		gsFileURI = gs.(string) + workDir + voiceFile
+		gsFileURI = gs.(string) + gsWorkDir + voiceFile
 	}
 
+	return
+}
+
+// uploadFile upload voice file to Google Cloud Storage
+func uploadFile() error {
+	f, err := os.Open(voiceFile)
+	if err != nil{
+		return err
+	}
+	defer f.Close()
+
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("failed to create GS client: %v", err)
+	}
+	wc := client.Bucket(gsBucketName).Object(gsWorkDir + voiceFile).NewWriter(ctx)
+	if _, err = io.Copy(wc, f); err != nil {
+		return err
+	}
+	if err := wc.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// reqCloudSpeechToText CloudSpeechToTextへのリクエストを実行する
+func reqCloudSpeechToText(w io.Writer, gcsURI string) error {
+	ctx := context.Background()
+	client, err := speech.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	// Detects speech in the audio file
+	// FLACエンコード、サンプリングレート44100Hz、日本語
+	req := &speechpb.LongRunningRecognizeRequest{
+		Config: &speechpb.RecognitionConfig{
+			Encoding:        speechpb.RecognitionConfig_FLAC,
+			SampleRateHertz: SAMPLING_RATE_HERTZ,
+			LanguageCode:    LANGUAGE_CODE,
+		},
+		Audio: &speechpb.RecognitionAudio{
+			AudioSource: &speechpb.RecognitionAudio_Uri{Uri: gcsURI},
+		},
+	}
+	op, err := client.LongRunningRecognize(ctx, req)
+	if err != nil {
+		return err
+	}
+	resp, err := op.Wait(ctx)
+	if err != nil {
+		return err
+	}
+	// Prints to the results.
+	for _, result := range resp.Results {
+		for _, alt := range result.Alternatives {
+			// confidenceが1.0に近いほど認識精度が高い
+			fmt.Fprintf(w, "\"%v\" (confidence=%3f)\n", alt.Transcript, alt.Confidence)
+		}
+	}
+	return nil
+}
+
+// exists ファイルの存在確認
+func exists(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil
+}
+
+// getVoiceFileName ファイルから拡張子を取り除いたファイル名を取得
+func getVoiceFileName(f string) string {
+	slist := strings.Split(f, ".")
+	return slist[0]
 }
